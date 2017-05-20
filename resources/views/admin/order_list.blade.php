437d81b45b8277f27cc00cc06a914f8afcbb38da
Christian d'Autume <christian.dautume@gmail.com>
2017-05-20 22:21:58 +0200

    - Fonct: Ajout d'un graphique de vente total (en plus des spécifiques)

--- resources/views/admin/order_list.blade.php
+++ resources/views/admin/order_list.blade.php
@@ -9,6 +9,20 @@
 @endsection
 
 @section('content')
+    <div class="alert alert-success" id="billet_ok" style="display: none">
+        <h4><i class="icon fa fa-check"></i> Billet validé !</h4>
+        Détenteur <strong id="identity"></strong>
+        <br>Options <strong id="options"></strong>
+    </div>
+    <div class="alert alert-danger" id="billet_wrong" style="display: none">
+        <h4><i class="icon fa fa-ban"></i> ATTENTION !</h4>
+        Le billet de <strong id="identity_error"></strong> a déja été validé <strong id="datetime"></strong>.
+    </div>
+    <div class="alert alert-warning" id="billet_inconnu" style="display: none">
+        <h4><i class="icon fa fa-ban"></i> INCONNU !</h4>
+        Le code n'est pas reconnu.
+    </div>
+
     <div class="box">
         <div class="box-header">
             <h3 class="box-title">Commandes</h3>
@@ -38,7 +52,7 @@
                                         <a
                                                 class="btn btn-info btn-xs" href="{{ route('admin_billet_view', ['billet'=>$billet]) }}"><i class="fa fa-eye"></i></a>
                                         @if(is_null($billet->validated_at))<a
-                                                class="btn btn-info btn-xs" href="{{ route('admin_billet_validate', ['id'=>$billet->id]) }}"><i class="fa fa-ticket"></i> Valider l'entrée</a>
+                                                class="btn btn-info btn-xs validate_link" data-href="{{ $billet->id }}"><i class="fa fa-ticket"></i> Valider l'entrée</a>
                                     @endif </li>
                                 @endforeach
                             </ul></td>
@@ -70,9 +84,83 @@
 @endsection
 @section('sublayout-js')
     <script>
-        $(function () {
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.11.1/typeahead.bundle.min.js"></script>
+    <script type="application/javascript">
+        var path = "{{ route('guichet_autocomplete') }}";
+
+        jQuery(document).ready(function($) {
+
+            //Show notifications
+            var billetValidated = function(obj){
+                $('#billet_ok').hide(100);
+                $('#billet_wrong').hide(100);
+                $('#billet_inconnu').hide(100);
+
+                $("#identity").html(obj.name + ' '+ obj.surname);
+                $("#options").html(obj.options);
+                $('#billet_ok').show(100);
+            };
+
+            var billetError = function(obj){
+                $('#billet_ok').hide(100);
+                $('#billet_wrong').hide(100);
+                $('#billet_inconnu').hide(100);
+
+                $("#identity_error").html(obj.name + ' '+ obj.surname);
+                $("#datetime").html(obj.validated_at);
+
+                $('#billet_wrong').show(100);
+            };
+
+            var billetInconnu = function(){
+                $('#billet_ok').hide(100);
+                $('#billet_wrong').hide(100);
+                $('#billet_inconnu').hide(100);
+
+                $('#billet_inconnu').show(100);
+            };
+
+            //Check request
+            var responseCheck = function (obj) {
+                switch (obj.validated) {
+                    case true:
+                        billetValidated(obj);
+                        break;
+                    case 'already':
+                        billetError(obj);
+                        break;
+                    case false:
+                    default:
+                        billetInconnu();
+                }
+            };
+            $('#search').change(function(event){
+                event.preventDefault();
+                $.get("{{ route('guichet_validate') }}", {code: $(this).val()})
+                    .done(function(data){
+                        responseCheck(data);
+                    });
+            });
+            $('#search').focus();
+
             $("#commandes").DataTable();
+
+            $('#commandes').on('draw.dt', function () {
+                setTimeout(function () {
+                    console.log('page change', $('.validate_link'));
+                    $('.validate_link').off('click').on('click', function() {
+                        console.log('CLIKED');
+                        $.get("{{ route('guichet_validate') }}",{id:$(this).attr('data-href')})
+                            .done(function(data){
+                                console.log('got res');
+                                responseCheck(data);
+                            });
+                    });
+                }, 50);
+            });
         });
     </script>
+
 @endsection
 
