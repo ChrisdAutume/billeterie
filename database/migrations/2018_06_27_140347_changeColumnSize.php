0312dc803bd12cea36cc1ae09ef3be281cec0c95
Christian d'Autume <christian@dautume.fr>
2018-06-27 14:12:28 +0200

    Fix DB schema: session data size and basked data size

---
+++ database/migrations/2018_06_27_140347_changeColumnSize.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeColumnSize extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('sessions', function (Blueprint $table) {
+            $table->longText('payload')->change();
+        });
+        Schema::table('orders', function (Blueprint $table) {
+            $table->longText('data')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('sessions', function (Blueprint $table) {
+            $table->text('payload')->change();
+        });
+        Schema::table('orders', function (Blueprint $table) {
+            $table->text('data')->change();
+        });
+    }
+}
