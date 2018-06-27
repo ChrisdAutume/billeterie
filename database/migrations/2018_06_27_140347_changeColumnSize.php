edcfe5c7020cb0aa8dd566dab33a3797c8cecc35
Christian d'Autume <christian@dautume.fr>
2018-06-27 14:22:44 +0200

    Fix DB schema: session data size and basked data size Uglly workaround

--- database/migrations/2018_06_27_140347_changeColumnSize.php
+++ database/migrations/2018_06_27_140347_changeColumnSize.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Migrations\Migration;
 
 class ChangeColumnSize extends Migration
 {
+    public function __construct()
+    {
+        DB::getDoctrineSchemaManager()->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'string');
+    }
     /**
      * Run the migrations.
      *
